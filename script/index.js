8109c428d548c3904c8b091495930b8842c6e99e
doppio-320 <anthonyjemo@gmail.com>
2024-12-04 21:00:35 +0800

    finished phoebe section

--- script/index.js
+++ script/index.js
@@ -1,12 +1,15 @@
 window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
     const parallaxImg = document.querySelector('#parallax-fg');
+    const parallaxBg = document.querySelector('#header-sect');
   
     // Adjust the parallax speed factor (e.g., 0.5 for half speed)
     const speedFactor = -0.2;
+    const bgSpeedFactor = 0.015;
   
     // Update the image position relative to scroll
     parallaxImg.style.transform = `translateY(${scrollTop * speedFactor}px) translateX(-50%)`;
+    parallaxBg.style.setProperty('--bgScroll', `${(scrollTop * bgSpeedFactor) - 10}%`);
 
     var blurAmt = ((10-0)/(700-100))*(scrollTop - 100);
     if(blurAmt<0) {
